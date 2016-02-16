6c92d35167f9a4d6bc2c1b1ec8a3f4e9e2545f57
Matt Hays <mdhays1995@gmail.com>
2016-02-16 09:46:14 -0600

    testing to push to heroku

---
+++ server.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const app = require('express')();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const routes = require('./routes/');
+
+const PORT = process.env.PORT || 3000;
+
+const MONGODB_HOST = process.env.MONGODB_HOST || 'localhost';
+const MONGODB_PORT = process.env.MONGODB_PORT || 27017;
+const MONGODB_USER = process.env.MONGODB_USER || '';
+const MONGODB_PASS = process.env.MONGODB_PASS || '';
+const MONGODB_NAME = process.env.MONGODB_NAME || 'stock-market-webapp';
+
+const MONGODB_AUTH = MONGODB_USER
+  ? `${MONGODB_USER}:${MONGODB_PASS}@`
+  : '';
+
+const MONGODB_URL = `mongodb://${MONGODB_AUTH}${MONGODB_HOST}:${MONGODB_PORT}/${MONGODB_NAME}`;
+
+app.set('view engine', 'jade');
+
+app.locals.title = 'Stock Market Webapp';
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(routes);
+
+mongoose.connect(MONGODB_URL);
+
+mongoose.connection.on('open', () => {
+  app.listen(PORT, () => {
+    console.log(`Node.js server started. Listening on port ${PORT}`);
+  });
+});
